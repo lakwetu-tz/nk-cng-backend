1340a7d40c6f29db6dceddfacd9e08c0c17fccf0
rigobert-art <rigobertkiata7@gmail.com>
2024-07-02 12:32:14 +0300

    For production

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,21 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import dotenv from 'dotenv';
-import morgan from 'morgan';
-import multer from 'multer';
-import cors from 'cors';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import multer from "multer";
+import cors from "cors";
 import http from "http";
-import SocketIO from 'socket.io';
-const cookieParser = require('cookie-parser');
-import bodyParser from 'body-parser';
+import SocketIO from "socket.io";
+const cookieParser = require("cookie-parser");
+import bodyParser from "body-parser";
 
-import userRoute from './route/userRoute';
-import superRoute from './route/superuserRoute'; 
-import loanRoute from './route/loanRoute';
-import formRoute from './route/formRoute';
-import guarantorRoute from './route/guarantorRoute';
-import vehicleRoute from './route/vehicleRoute';
-import Form from './model/formModel';
+import userRoute from "./route/userRoute";
+import superRoute from "./route/superuserRoute";
+import loanRoute from "./route/loanRoute";
+import formRoute from "./route/formRoute";
+import guarantorRoute from "./route/guarantorRoute";
+import vehicleRoute from "./route/vehicleRoute";
+import Form from "./model/formModel";
 
 dotenv.config();
 
@@ -23,56 +23,61 @@ const app = express();
 const port = process.env.PORT || 4000;
 // Middleware
 app.use(bodyParser.json());
-app.disable("x-powered-by")
-app.use(morgan("dev"))
-app.use(express.urlencoded({ extended: true }))
-app.use(express.json())
-app.use(cors({ origin: '*' }))
-app.use(cookieParser())
+app.disable("x-powered-by");
+app.use(morgan("dev"));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(cors({origin: "*"}));
+app.use(cookieParser());
 
 const server = http.createServer(app);
 const io = new SocketIO.Server(server, {
-    cors: {
-        origin: '*',
-    },
+	cors: {
+		origin: "*",
+	},
 });
 app.set("io", io);
 
-//routes 
-app.use('/api/v1/user', userRoute);
-app.use('/api/v1/superuser', superRoute);
-app.use('/api/v1/loan', loanRoute);
+//routes
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/superuser", superRoute);
+app.use("/api/v1/loan", loanRoute);
 // app.use('/api/v1/sms', smsRoute);
-app.use('/api/v1/form', formRoute);
-app.use('/api/v1/guarantor', guarantorRoute);
-app.use('/api/v1/vehicle', vehicleRoute);
-
+app.use("/api/v1/form", formRoute);
+app.use("/api/v1/guarantor", guarantorRoute);
+app.use("/api/v1/vehicle", vehicleRoute);
 
 // Error handling middleware
-app.use((err: any, req: express.Request, res: express.Response, next: express.NextFunction) => {
-    const statusCode = err.statusCode || 500;
-    const message = err.message || 'Internal Server Error';
+app.use(
+	(
+		err: any,
+		req: express.Request,
+		res: express.Response,
+		next: express.NextFunction
+	) => {
+		const statusCode = err.statusCode || 500;
+		const message = err.message || "Internal Server Error";
 
-    res.status(statusCode).json({
-        error: {
-            status: statusCode,
-            message: message,
-        },
-    });
-});
-
+		res.status(statusCode).json({
+			error: {
+				status: statusCode,
+				message: message,
+			},
+		});
+	}
+);
 
 // Connect to MongoDB and start server
-mongoose.connect(process.env.MONGO_URI || "", {
-    family: 4,
-    serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
-})
-    .then(() => {
-        app.listen(4000, () => {
-            
-            console.log(`Server running on port ${port}`);
-        });
-    })
-    .catch((error: any) => {
-        console.error('Error connecting to MongoDB:', error);
-});
+mongoose
+	.connect(process.env.MONGO_URI || "", {
+		family: 4,
+		serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+	})
+	.then(() => {
+		app.listen(port, () => {
+			console.log(`Server running on port ${port}`);
+		});
+	})
+	.catch((error: any) => {
+		console.error("Error connecting to MongoDB:", error);
+	});
